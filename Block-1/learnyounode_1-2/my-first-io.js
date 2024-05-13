1ced2d8e8d28430a833b2f48499308c720f32154
Petr Cerny <petr.cerny@e3ag.ch>
2024-05-13 09:37:54 +0000

    my-first-io

---
+++ Block-1/learnyounode_1-2/my-first-io.js
@@ -0,0 +1,9 @@
+import * as fs from 'node:fs/promises';
+import * as fs from 'node:fs';
+import
+
+const fs = require('fs')
+fs.readFile('my-first-io.txt', (err, inputD) => {
+   
+   console.log(inputD.toString());
+});
