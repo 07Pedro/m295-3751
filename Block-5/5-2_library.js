69b43b9e2eb4cbab83a1d50ae612ff5c68f6dce6
Petr Cerny <petr.cerny@e3ag.ch>
2024-05-21 16:33:01 +0200

    ESlint rework

--- Block-5/5-2_library.js
+++ Block-5/5-2_library.js
@@ -3,68 +3,68 @@ const express = require('express')
 const app = express()
 const port = 3001
 
-const books = [
-  {
-    isbn: '978-3-16-148410-0',
-    title: 'Das Leben des Galileo',
-    year: 2018,
-    author: 'Bertolt Brecht'
-  },
-  {
-    isbn: '978-0-7432-7356-5',
-    title: 'Der Alchemist',
-    year: 1988,
-    author: 'Paulo Coelho'
-  },
-  {
-    isbn: '978-1-56619-909-4',
-    title: 'Das Kapital',
-    year: 1867,
-    author: 'Karl Marx'
-  },
-  {
-    isbn: '978-0-452-28423-4',
-    title: '1984',
-    year: 1949,
-    author: 'George Orwell'
-  },
-  {
-    isbn: '978-0-7432-7356-6',
-    title: 'Ein kurzer Abstieg',
-    year: 2003,
-    author: 'John Doe'
-  },
-  {
-    isbn: '978-0-399-14372-4',
-    title: 'Fahrenheit 451',
-    year: 1953,
-    author: 'Ray Bradbury'
-  },
-  {
-    isbn: '978-0-14-028333-4',
-    title: 'Die Verwandlung',
-    year: 1915,
-    author: 'Franz Kafka'
-  },
-  {
-    isbn: '978-0-452-27750-4',
-    title: 'Schöne neue Welt',
-    year: 1932,
-    author: 'Aldous Huxley'
-  },
-  {
-    isbn: '978-0-553-21311-7',
-    title: 'Der Herr der Ringe',
-    year: 1954,
-    author: 'J.R.R. Tolkien'
-  },
-  {
-    isbn: '978-0-7432-7357-2',
-    title: 'Die Stadt der träumenden Bücher',
-    year: 2004,
-    author: 'Walter Moers'
-  }
-]
+// const books = [
+//   {
+//     isbn: '978-3-16-148410-0',
+//     title: 'Das Leben des Galileo',
+//     year: 2018,
+//     author: 'Bertolt Brecht'
+//   },
+//   {
+//     isbn: '978-0-7432-7356-5',
+//     title: 'Der Alchemist',
+//     year: 1988,
+//     author: 'Paulo Coelho'
+//   },
+//   {
+//     isbn: '978-1-56619-909-4',
+//     title: 'Das Kapital',
+//     year: 1867,
+//     author: 'Karl Marx'
+//   },
+//   {
+//     isbn: '978-0-452-28423-4',
+//     title: '1984',
+//     year: 1949,
+//     author: 'George Orwell'
+//   },
+//   {
+//     isbn: '978-0-7432-7356-6',
+//     title: 'Ein kurzer Abstieg',
+//     year: 2003,
+//     author: 'John Doe'
+//   },
+//   {
+//     isbn: '978-0-399-14372-4',
+//     title: 'Fahrenheit 451',
+//     year: 1953,
+//     author: 'Ray Bradbury'
+//   },
+//   {
+//     isbn: '978-0-14-028333-4',
+//     title: 'Die Verwandlung',
+//     year: 1915,
+//     author: 'Franz Kafka'
+//   },
+//   {
+//     isbn: '978-0-452-27750-4',
+//     title: 'Schöne neue Welt',
+//     year: 1932,
+//     author: 'Aldous Huxley'
+//   },
+//   {
+//     isbn: '978-0-553-21311-7',
+//     title: 'Der Herr der Ringe',
+//     year: 1954,
+//     author: 'J.R.R. Tolkien'
+//   },
+//   {
+//     isbn: '978-0-7432-7357-2',
+//     title: 'Die Stadt der träumenden Bücher',
+//     year: 2004,
+//     author: 'Walter Moers'
+//   }
+// ]
 
 const lends = [
   {
@@ -163,7 +163,7 @@ const lendData = {
   returned_at: ''
 }
 
-const time = Date.now()
+// const time = Date.now()
 console.log(Date.now())
 
 app.post('/lends', (request, response) => {
