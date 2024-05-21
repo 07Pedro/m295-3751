43b15946d05e148b9fa4df780c1a4e5e7a463dae
Petr Cerny <petr.cerny@e3ag.ch>
2024-05-21 15:37:15 +0200

    ESlint

--- Block-7/7-2-express_session.js
+++ Block-7/7-2-express_session.js
@@ -1,34 +1,34 @@
-const session = require('express-session');
-const express = require('express');
-const sessionstorage = require('sessionstorage');
-const app = express();
-const port = 3001;
+const session = require('express-session')
+const express = require('express')
+const sessionstorage = require('sessionstorage')
+const app = express()
+const port = 3001
 
 app.use(session({
-    secret: 'keyboardcat',
-    resave: true,
-    saveUninitialized: true,
-    cookie: { secure: true }
+  secret: 'keyboardcat',
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: true }
 }))
 
-app.post('/:name', function(req, res) {
-    const name = req.params.name;
-    req.session.name = name;
+app.post('/:name', function (req, res) {
+  const name = req.params.name
+  req.session.name = name
 
-    res.send('Hello ' + req.session.name);
+  res.send('Hello ' + req.session.name)
 })
 
-app.get('/name', function(req, res) {
-    let thesessionname = req.session.name;
+app.get('/name', function (req, res) {
+  const thesessionname = req.session.name
 
-    res.send(thesessionname);
+  res.send(thesessionname)
 })
 
-app.delete('/:name', function(req, res) {
-    sessionstorage.removeItem(req.session.name);
-    res.send(req.session.name);
+app.delete('/:name', function (req, res) {
+  sessionstorage.removeItem(req.session.name)
+  res.send(req.session.name)
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-});
+  console.log(`Example app listening on port ${port}`)
+})
